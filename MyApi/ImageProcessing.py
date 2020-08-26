4d69c85a745609a70bbd42a17928e785d3f496e4
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-08-26 18:06:43 +0530

    Few minor Changes

--- MyApi/ImageProcessing.py
+++ MyApi/ImageProcessing.py
@@ -2,7 +2,12 @@ import cv2
 
 image = cv2.imread("static/images/svg1.png")
 
+text = "Pratyaksh"
+text_image = cv2.putText(image, text, (0,30), cv2.FONT_HERSHEY_COMPLEX,1, (255,0,0),1)
+text_box_image = cv2.rectangle(text_image,(30,30),(200,200),(0,255,0),2)
+
 while True:
- cv2.imshow("This is my Image",image)
- if cv2.waitKey(1) == ord("q"):
-     break
+    cv2.imshow("Default Image", text_box_image)
+    if cv2.waitKey(0) == ord('q'):
+        # cv2.destroyAllWindows()
+        break
