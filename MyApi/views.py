03e455482253fc1f981fe873a267fdb0a40a93f9
Pratyaksh7 <pratyakshgupta7@gmail.com>
2020-08-26 20:57:44 +0530

    Settings for media files
    Changes in Views and Urls

--- MyApi/views.py
+++ MyApi/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import MyFile
 
-# Create your views here.
 def index(request):
+    if request.method == "POST":
+        img = request.FILES['image']
+        data = MyFile.objects.create(image = img)   # image is the column name in the table(Model) in database
+        url = "http://127.0.0.1:8000" + data.image.url
+        return redirect(url)
+
     return render(request, "index.html")
